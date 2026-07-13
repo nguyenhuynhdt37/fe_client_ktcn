5d889746a7220755a57e35bd29dc8351f53b3036
nguyenhuynh04 <nguyenhuynhdt37@gmail.com>
2026-07-13 14:35:09 +0700

    chore: use static public verification file and update proxy exclusion

--- src/app/googled15a89c91c8574e3.html/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return new NextResponse("google-site-verification: googled15a89c91c8574e3", {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-}
